1561091855790db7cd3a746bb4b1c4e3aed05620
asuravadha <rr.easwar@gmail.com>
2021-12-02 10:33:05 +0530

    Add project files.

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        public void login(object l,EventArgs e)
+        {
+            bool ISEmailEmpty = string.IsNullOrEmpty(Username.Text);
+            bool ISPwdEmpty=string.IsNullOrEmpty(Password.Text);
+            if(ISEmailEmpty || ISPwdEmpty)
+            {
+
+            }
+            else
+            {
+                Navigation.PushAsync(new HomePage());
+
+            }
+        }
+        
+    }
+}
